5b862ed064d5e5ddeeceac5f4c1b8a33c12166a7
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2022-12-30 15:33:30 +0100

    Day-09 solution

---
+++ src/test/java/com/example/adventofcode/day09/RopeBridgeTest.java
@@ -0,0 +1,41 @@
+package com.example.adventofcode.day09;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RopeBridgeTest {
+    private static Stream<Arguments> filePathsRopeLengthAndExpectedPositionsTouchedByRopeTail() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/day09/example_input",
+                        2,
+                        13
+                ),
+                Arguments.of("src/main/java/com/example/adventofcode/day09/input",
+                        2,
+                        6357
+                ),
+                Arguments.of("src/main/java/com/example/adventofcode/day09/example_input",
+                        10,
+                        36
+                ),
+                Arguments.of("src/main/java/com/example/adventofcode/day09/input",
+                        10,
+                        2627
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filePathsRopeLengthAndExpectedPositionsTouchedByRopeTail")
+    void testCalculatePositionsTouchedByRopeTail(final String fileName,
+                                                 final int ropeLength,
+                                                 final int expectedPositionsTouchedByRopeTail) throws IOException {
+        assertEquals(expectedPositionsTouchedByRopeTail, RopeBridge.calculatePositionsTouchedByRopeTail(fileName, ropeLength));
+    }
+}
