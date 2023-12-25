a7fb86cf037f6e3e5fd5841f2c0281d4d97e876b
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2023-12-25 17:10:22 +0100

    [year2023-day23] Solution

---
+++ src/main/java/com/example/adventofcode/year2023/day23/ALongWalkBruteForce.java
@@ -0,0 +1,172 @@
+package com.example.adventofcode.year2023.day23;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class ALongWalkBruteForce {
+    private static final String FILENAME = "src/main/java/com/example/adventofcode/year2023/day23/input";
+    private static final String EXAMPLE_FILENAME = "src/main/java/com/example/adventofcode/year2023/day23/example_input";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(calculateLongestHike(EXAMPLE_FILENAME));
+        //94
+        System.out.println(calculateLongestHike(FILENAME));
+        //2170
+        System.out.println(calculateLongestHikeWithoutSlopes(EXAMPLE_FILENAME));
+        //154
+    }
+
+    record Point(int x, int y) {
+    }
+
+    record State(Point point, List<Point> history) {
+    }
+
+    private static final List<Point> DIRECTIONS = List.of(
+            new Point(0, 1),
+            new Point(0, -1),
+            new Point(1, 0),
+            new Point(-1, 0)
+    );
+
+    public static int calculateLongestHike(final String filename) throws IOException {
+        List<String> lines = readLines(filename);
+        List<List<Character>> map = obtainMap(lines);
+
+        Point start = new Point(1, 0);
+        Point end = new Point(map.getLast().size() - 2, map.size() - 1);
+        return calculateLongestDistanceBetween(start, map, end);
+    }
+
+    public static long calculateLongestHikeWithoutSlopes(final String filename) throws IOException {
+        List<String> lines = readLines(filename);
+        List<List<Character>> map = obtainMap(lines);
+
+        Point start = new Point(1, 0);
+        Point end = new Point(map.getLast().size() - 2, map.size() - 1);
+        return calculateLongestDistanceWithoutSlopesBetween(start, map, end);
+    }
+
+    private static List<String> readLines(String filename) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
+
+    private static List<List<Character>> obtainMap(List<String> inputLines) {
+        List<List<Character>> map = new ArrayList<>();
+        for (String line : inputLines) {
+            char[] splitted = line.toCharArray();
+            List<Character> lavaLine = new ArrayList<>();
+            for (char c : splitted) {
+                lavaLine.add(c);
+            }
+            map.add(lavaLine);
+        }
+        return map;
+    }
+
+    private static int calculateLongestDistanceBetween(Point start, List<List<Character>> map, Point end) {
+        int maxPath = 0;
+        Queue<State> queue = new ArrayDeque<>();
+        List<Point> history = new ArrayList<>();
+        history.add(start);
+        queue.add(new State(start, history));
+
+        while (!queue.isEmpty()) {
+            State poll = queue.poll();
+
+            for (Point direction : DIRECTIONS) {
+                Point newPoint = new Point(poll.point.x + direction.x, poll.point.y + direction.y);
+                if (isValid(newPoint, map) && !poll.history.contains(newPoint)) {
+                    if (map.get(newPoint.y).get(newPoint.x).equals('#')
+                            || isStepInWrongDirection(direction, poll, map)) {
+                        continue;
+                    }
+
+                    if (newPoint.equals(end)) {
+                        if (poll.history.size() + 1 > maxPath) {
+                            maxPath = poll.history.size() + 1;
+                        }
+                    } else {
+                        List<Point> newHistory = new ArrayList<>(poll.history);
+                        newHistory.add(newPoint);
+                        queue.add(new State(newPoint, newHistory));
+                    }
+                }
+            }
+        }
+
+        return maxPath - 1;
+    }
+
+    private static boolean isValid(Point newPoint, List<List<Character>> map) {
+        return newPoint.y >= 0 && newPoint.y < map.size()
+                && newPoint.x >= 0 && newPoint.x < map.getFirst().size();
+    }
+
+    private static boolean isStepInWrongDirection(Point direction, State poll, List<List<Character>> map) {
+        if (!poll.history.isEmpty() && (map.get(poll.history.getLast().y).get(poll.history.getLast().x).equals('>')
+                && !direction.equals(new Point(1, 0)))) {
+            return true;
+        }
+        if (!poll.history.isEmpty() && (map.get(poll.history.getLast().y).get(poll.history.getLast().x).equals('<')
+                && !direction.equals(new Point(-1, 0)))) {
+            return true;
+        }
+        if (!poll.history.isEmpty() && (map.get(poll.history.getLast().y).get(poll.history.getLast().x).equals('v')
+                && !direction.equals(new Point(0, 1)))) {
+            return true;
+        }
+        if (!poll.history.isEmpty() && (map.get(poll.history.getLast().y).get(poll.history.getLast().x).equals('^')
+                && !direction.equals(new Point(0, -1)))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int calculateLongestDistanceWithoutSlopesBetween(Point start, List<List<Character>> map, Point end) {
+        int maxPath = 0;
+        Queue<State> queue = new ArrayDeque<>();
+        List<Point> history = new ArrayList<>();
+        history.add(start);
+        queue.add(new State(start, history));
+
+        while (!queue.isEmpty()) {
+            State poll = queue.poll();
+
+            for (Point direction : DIRECTIONS) {
+                Point newPoint = new Point(poll.point.x + direction.x, poll.point.y + direction.y);
+                if (isValid(newPoint, map) && !poll.history.contains(newPoint)) {
+                    if (map.get(newPoint.y).get(newPoint.x).equals('#')) {
+                        continue;
+                    }
+
+                    if (newPoint.equals(end)) {
+                        if (poll.history.size() + 1 > maxPath) {
+                            maxPath = poll.history.size() + 1;
+                        }
+                    } else {
+                        List<Point> newHistory = new ArrayList<>(poll.history);
+                        newHistory.add(newPoint);
+                        queue.add(new State(newPoint, newHistory));
+                    }
+                }
+            }
+        }
+
+        return maxPath - 1;
+    }
+}
---
+++ src/test/java/com/example/adventofcode/year2023/day23/ALongWalkTest.java
@@ -0,0 +1,41 @@
+package com.example.adventofcode.year2023.day23;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ALongWalkTest {
+
+    private static Stream<Arguments> filepathsAndExpectedLongestHike() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day23/example_input", 94),
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day23/input", 2170)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filepathsAndExpectedLongestHike")
+    void calculateLongestHike(final String filename,
+                              final int expectedLongestHike) throws IOException {
+        assertEquals(expectedLongestHike, ALongWalk.calculateLongestHike(filename));
+    }
+
+    private static Stream<Arguments> filepathsAndExpectedLongestHikeWithoutSlopes() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day23/example_input", 154),
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day23/input", 6502)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filepathsAndExpectedLongestHikeWithoutSlopes")
+    void calculateLongestHikeWithoutSlopes(final String filename,
+                                           final int expectedLongestHike) throws IOException {
+        assertEquals(expectedLongestHike, ALongWalk.calculateLongestHikeWithoutSlopes(filename));
+    }
+}
