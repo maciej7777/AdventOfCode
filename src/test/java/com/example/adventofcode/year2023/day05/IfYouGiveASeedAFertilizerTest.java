72f7ce9915da76867b82683a52689e37949e496d
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2023-12-28 12:38:59 +0100

    [year2023-day05] Solution

---
+++ src/test/java/com/example/adventofcode/year2023/day05/IfYouGiveASeedAFertilizerTest.java
@@ -0,0 +1,41 @@
+package com.example.adventofcode.year2023.day05;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IfYouGiveASeedAFertilizerTest {
+
+    private static Stream<Arguments> filepathsAndExpectedLowestLocationNumber() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day05/example_input", 35),
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day05/input", 650599855)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filepathsAndExpectedLowestLocationNumber")
+    void calculateLowestLocationNumber(final String filename,
+                                       final int expectedLowestLocationNumber) throws IOException {
+        assertEquals(expectedLowestLocationNumber, IfYouGiveASeedAFertilizer.calculateLowestLocationNumber(filename));
+    }
+
+    private static Stream<Arguments> filepathsAndExpectedLowestLocationNumberFromRanges() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day05/example_input", 46),
+                Arguments.of("src/main/java/com/example/adventofcode/year2023/day05/input", 1240035)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filepathsAndExpectedLowestLocationNumberFromRanges")
+    void calculateLowestLocationNumberFromRanges(final String filename,
+                                                 final int expectedLowestLocationNumber) throws IOException {
+        assertEquals(expectedLowestLocationNumber, IfYouGiveASeedAFertilizer.calculateLowestLocationNumberFromRanges(filename));
+    }
+}
