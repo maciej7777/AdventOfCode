bf14b49f66a668db00353cea2566190a78ebec09
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2024-12-28 11:45:33 +0100

    [year2024day08] Refactoring

--- src/main/java/com/example/adventofcode/year2024/day08/ResonantCollinearity.java
+++ src/main/java/com/example/adventofcode/year2024/day08/ResonantCollinearity.java
@@ -1,76 +1,45 @@
 package com.example.adventofcode.year2024.day08;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-//TODO refactor this class
-public class Day08 {
+public class ResonantCollinearity {
     private static final String FILENAME = "src/main/java/com/example/adventofcode/year2024/day08/input";
     private static final String EXAMPLE_FILENAME = "src/main/java/com/example/adventofcode/year2024/day08/example_input";
 
     public static void main(String[] args) throws IOException {
-        System.out.println(calculateSolution1(EXAMPLE_FILENAME));
-        System.out.println(calculateSolution1(FILENAME));
-        System.out.println(calculateSolution2(EXAMPLE_FILENAME));
-        System.out.println(calculateSolution2(FILENAME));
+        System.out.println(countAntinodeLocations(EXAMPLE_FILENAME));
+        System.out.println(countAntinodeLocations(FILENAME));
+        System.out.println(countAntinodeLocationsWithResonantHarmonics(EXAMPLE_FILENAME));
+        System.out.println(countAntinodeLocationsWithResonantHarmonics(FILENAME));
     }
 
 
     record Point(int x, int y) {
     }
 
-    public static long calculateSolution1(final String filename) throws IOException {
+    private record Input(HashMap<Character, List<Point>> mapRepresentation, int maxX, int maxY) {
+    }
+
+    public static int countAntinodeLocations(final String filename) throws IOException {
         List<String> lines = readLines(filename);
-
-        HashMap<Character, List<Point>> mapRepresentation = new HashMap<>();
-        int maxX = 0;
-        int maxY = 0;
-        for (String line : lines) {
-            maxX = line.length() - 1;
-            for (int i = 0; i < line.length(); i++) {
-                char currentElement = line.charAt(i);
-                if (currentElement != '.') {
-                    List<Point> currentPoints = mapRepresentation.getOrDefault(currentElement, new ArrayList<>());
-                    currentPoints.add(new Point(i, maxY));
-                    mapRepresentation.put(currentElement, currentPoints);
-                }
-            }
-            maxY++;
-        }
-        maxY--;
-
-
-        Set<Point> locations = new HashSet<>();
-        for (List<Point> positions : mapRepresentation.values()) {
-            for (int i = 0; i < positions.size(); i++) {
-                for (int j = i + 1; j < positions.size(); j++) {
-                    int diffX = positions.get(i).x - positions.get(j).x;
-                    int diffY = positions.get(i).y - positions.get(j).y;
-
-                    int newX1 = positions.get(i).x + diffX;
-                    int newY1 = positions.get(i).y + diffY;
-
-                    if (isValidPoint(newX1, newY1, maxX, maxY)) {
-                        locations.add(new Point(newX1, newY1));
-                    }
-
-                    int newX2 = positions.get(j).x - diffX;
-                    int newY2 = positions.get(j).y - diffY;
-
-                    if (isValidPoint(newX2, newY2, maxX, maxY)) {
-                        locations.add(new Point(newX2, newY2));
-                    }
-                }
-            }
-        }
+        Input input = parseInput(lines);
+        Set<Point> locations = calculateAntinodeLocations(input);
 
         return locations.size();
     }
 
-    private static boolean isValidPoint(int newX1, int newY1, int maxX, int maxY) {
-        return newX1 >= 0 && newX1 <= maxX && newY1 >= 0 && newY1 <= maxY;
+    public static int countAntinodeLocationsWithResonantHarmonics(final String filename) throws IOException {
+        List<String> lines = readLines(filename);
+        Input input = parseInput(lines);
+
+        Set<Point> locations = calculateAntinodeLocationsWithResonantHarmonics(input);
+
+        return locations.size();
     }
 
     private static List<String> readLines(String filename) throws IOException {
@@ -85,9 +54,7 @@ public class Day08 {
         return lines;
     }
 
-    public static long calculateSolution2(final String filename) throws IOException {
-        List<String> lines = readLines(filename);
-
+    private static Input parseInput(List<String> lines) {
         HashMap<Character, List<Point>> mapRepresentation = new HashMap<>();
         int maxX = 0;
         int maxY = 0;
@@ -103,11 +70,13 @@ public class Day08 {
             }
             maxY++;
         }
-        maxY--;
 
+        return new Input(mapRepresentation, maxX, maxY - 1);
+    }
 
+    private static Set<Point> calculateAntinodeLocations(Input input) {
         Set<Point> locations = new HashSet<>();
-        for (List<Point> positions : mapRepresentation.values()) {
+        for (List<Point> positions : input.mapRepresentation().values()) {
             for (int i = 0; i < positions.size(); i++) {
                 for (int j = i + 1; j < positions.size(); j++) {
                     int diffX = positions.get(i).x - positions.get(j).x;
@@ -115,7 +84,38 @@ public class Day08 {
 
                     int newX1 = positions.get(i).x + diffX;
                     int newY1 = positions.get(i).y + diffY;
-                    while (isValidPoint(newX1, newY1, maxX, maxY)) {
+
+                    if (isValidPoint(newX1, newY1, input.maxX(), input.maxY())) {
+                        locations.add(new Point(newX1, newY1));
+                    }
+
+                    int newX2 = positions.get(j).x - diffX;
+                    int newY2 = positions.get(j).y - diffY;
+
+                    if (isValidPoint(newX2, newY2, input.maxX(), input.maxY())) {
+                        locations.add(new Point(newX2, newY2));
+                    }
+                }
+            }
+        }
+        return locations;
+    }
+
+    private static boolean isValidPoint(int newX1, int newY1, int maxX, int maxY) {
+        return newX1 >= 0 && newX1 <= maxX && newY1 >= 0 && newY1 <= maxY;
+    }
+
+    private static Set<Point> calculateAntinodeLocationsWithResonantHarmonics(Input input) {
+        Set<Point> locations = new HashSet<>();
+        for (List<Point> positions : input.mapRepresentation.values()) {
+            for (int i = 0; i < positions.size(); i++) {
+                for (int j = i + 1; j < positions.size(); j++) {
+                    int diffX = positions.get(i).x - positions.get(j).x;
+                    int diffY = positions.get(i).y - positions.get(j).y;
+
+                    int newX1 = positions.get(i).x + diffX;
+                    int newY1 = positions.get(i).y + diffY;
+                    while (isValidPoint(newX1, newY1, input.maxX, input.maxY)) {
                         locations.add(new Point(newX1, newY1));
                         newX1 += diffX;
                         newY1 += diffY;
@@ -123,8 +123,7 @@ public class Day08 {
 
                     int newX2 = positions.get(j).x - diffX;
                     int newY2 = positions.get(j).y - diffY;
-
-                    while (isValidPoint(newX2, newY2, maxX, maxY)) {
+                    while (isValidPoint(newX2, newY2, input.maxX, input.maxY)) {
                         locations.add(new Point(newX2, newY2));
                         newX2 -= diffX;
                         newY2 -= diffY;
@@ -136,7 +135,6 @@ public class Day08 {
                 locations.addAll(positions);
             }
         }
-
-        return locations.size();
+        return locations;
     }
 }
---
+++ src/test/java/com/example/adventofcode/year2024/day08/ResonantCollinearityTest.java
@@ -0,0 +1,41 @@
+package com.example.adventofcode.year2024.day08;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResonantCollinearityTest {
+
+    private static Stream<Arguments> filepathsAndExpectedAntinodeLocations() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/year2024/day08/example_input", 14),
+                Arguments.of("src/main/java/com/example/adventofcode/year2024/day08/input", 259)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filepathsAndExpectedAntinodeLocations")
+    void countAntinodeLocations(final String filename,
+                                final int expectedAntinodeLocations) throws IOException {
+        assertEquals(expectedAntinodeLocations, ResonantCollinearity.countAntinodeLocations(filename));
+    }
+
+    private static Stream<Arguments> filepathsAndExpectedAntinodeLocationsWithResonantHarmonics() {
+        return Stream.of(
+                Arguments.of("src/main/java/com/example/adventofcode/year2024/day08/example_input", 34),
+                Arguments.of("src/main/java/com/example/adventofcode/year2024/day08/input", 927)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("filepathsAndExpectedAntinodeLocationsWithResonantHarmonics")
+    void countAntinodeLocationsWithResonantHarmonics(final String filename,
+                                                     final int expectedCalibrationNumber) throws IOException {
+        assertEquals(expectedCalibrationNumber, ResonantCollinearity.countAntinodeLocationsWithResonantHarmonics(filename));
+    }
+}
