cba278c32c4dff658dd4414b89fa46d1ee3246c3
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2025-01-06 13:47:02 +0100

    [year2024day20] Move input data

--- src/test/java/com/example/adventofcode/year2024/day20/RaceConditionTest.java
+++ src/test/java/com/example/adventofcode/year2024/day20/RaceConditionTest.java
@@ -12,11 +12,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RaceConditionTest {
     private static Stream<Arguments> filepathsAndCheatLengthAndSavedTimeAndExpectedCheats() {
         return Stream.of(
-                Arguments.of("src/main/java/com/example/adventofcode/year2024/day20/example_input", 2, 12, 8),
-                Arguments.of("src/main/java/com/example/adventofcode/year2024/day20/input", 2, 100, 1381),
-                Arguments.of("src/main/java/com/example/adventofcode/year2024/day20/example_input", 20, 72, 29),
-                Arguments.of("src/main/java/com/example/adventofcode/year2024/day20/example_input", 20, 76, 3),
-                Arguments.of("src/main/java/com/example/adventofcode/year2024/day20/input", 20, 100, 982124)
+                Arguments.of("AdventOfCodeData/2024/day20/example_input", 2, 12, 8),
+                Arguments.of("AdventOfCodeData/2024/day20/input", 2, 100, 1381),
+                Arguments.of("AdventOfCodeData/2024/day20/example_input", 20, 72, 29),
+                Arguments.of("AdventOfCodeData/2024/day20/example_input", 20, 76, 3),
+                Arguments.of("AdventOfCodeData/2024/day20/input", 20, 100, 982124)
         );
     }
 
